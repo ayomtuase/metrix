e9d218df7334636e5cd164557f538a34b5fcc7aa
Ayomide Oguntuase <oguntuase3@gmail.com>
2024-04-10 10:17:55 +0100

    navs committed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,89 @@
+"use client";
+
 import { CartIcon } from "@/components/icons/cart-icon";
 import { CustomersIcon } from "@/components/icons/customers-icon";
 import { FolderIcon } from "@/components/icons/folder-icon";
 import { OrdersIcon } from "@/components/icons/orders-icon";
 import { SalesChartIcon } from "@/components/icons/sales-chart-icon";
 import { cn } from "@/lib/utils";
+import {
+  ArcElement,
+  BarElement,
+  CategoryScale,
+  Chart as ChartJS,
+  LinearScale,
+  Tooltip,
+} from "chart.js";
 import Image from "next/image";
 import { Fragment } from "react";
+import { Bar, Doughnut } from "react-chartjs-2";
 import { RxCaretDown } from "react-icons/rx";
+
+ChartJS.register(ArcElement, CategoryScale, LinearScale, BarElement, Tooltip);
+
+ChartJS.defaults.elements.arc.borderWidth = 8;
+ChartJS.defaults.elements.arc.spacing = 0;
+
+const barOptions = {
+  responsive: true,
+};
+
+const barLabels = [
+  "Sep 10",
+  "Sep 11",
+  "Sep 12",
+  "Sep 13",
+  "Sep 14",
+  "Sep 15",
+  "Sep 16",
+];
+
+const barData = {
+  labels: barLabels,
+  datasets: [
+    {
+      data: [90, 40, 65, 20, 80, 40, 80],
+      backgroundColor: "hsl(230, 85%, 64%)",
+    },
+  ],
+  options: {
+    elements: {
+      bar: {
+        width: 13,
+      },
+    },
+  },
+};
+
+const doughnutData = {
+  datasets: [
+    {
+      data: [55, 35, 10],
+      backgroundColor: [
+        "hsla(230, 85%, 64%, 1)",
+        "hsla(32, 100%, 78%, 1)",
+        "hsla(230, 68%, 76%, 1)",
+      ],
+      borderColor: "hsla(230, 55%, 96%, 1)",
+      borderWidth: 16,
+      // options: {
+      //   elements: {
+      //     arc: {
+      //       borderWidth: 8,
+      //       spacing: 0,
+      //     },
+      //   },
+      // },
+    },
+  ],
+};
+
+const doughnutOptions = {};
 
 export default function Home() {
   return (
     <>
-      <div className="grid grid-cols-3 gap-[19px] h-[145px]">
+      <div className="grid grid-cols-1 lg:grid-cols-3 gap-[19px] lg:h-[145px]">
         <div className="group transition rounded-lg bg-white hover:bg-primary px-[15px] py-[11px] h-full flex flex-col justify-between">
           <div className="flex w-full justify-between items-center">
             <span className="h-9 w-9 grid place-items-center bg-[hsla(230,85%,64%,0.12)] group-hover:bg-[hsla(0,0%,100%,0.16)] text-primary group-hover:text-white rounded-lg">
@@ -22,7 +94,7 @@ export default function Home() {
               <RxCaretDown />
             </div>
           </div>
-          <div className="flex items-center group-hover:text-white">
+          <div className="flex items-center group-hover:text-white mt-3">
             <div className="flex flex-col space-y-2 grow">
               <p className="text-sm text-gray-text group-hover:text-white">
                 Sales
@@ -52,7 +124,7 @@ export default function Home() {
               <RxCaretDown />
             </div>
           </div>
-          <div className="flex items-center group-hover:text-white">
+          <div className="flex items-center group-hover:text-white mt-3">
             <div className="flex flex-col space-y-2 grow">
               <p className="text-sm text-gray-text group-hover:text-white">
                 Customers
@@ -87,7 +159,7 @@ export default function Home() {
               <RxCaretDown />
             </div>
           </div>
-          <div className="flex items-center group-hover:text-white">
+          <div className="flex items-center group-hover:text-white mt-3">
             <div className="flex flex-col space-y-2 grow">
               <p className="text-sm text-gray-text group-hover:text-white">
                 All Orders
@@ -111,18 +183,18 @@ export default function Home() {
       </div>
 
       <div className="grow mt-[19px]">
-        <div className="grid grid-cols-3 gap-[19px] h-full w-full">
-          <div className="col-span-2">
-            <div className="grid grid-cols-2 grid-rows-[145px_145px] gap-[19px]">
-              <div className="col-span-1 row-span-2 group transition rounded-lg bg-white hover:bg-primary px-[15px] py-[11px]">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-[19px] h-full w-full">
+          <div className="md:col-span-2">
+            <div className="grid grid-cols-1 md:grid-cols-2 grid-rows-[145px_145px_145px] md:grid-rows-[145px_145px] gap-[19px]">
+              <div className="col-span-1 row-span-2 transition rounded-lg bg-white px-[15px] py-[11px]">
                 <div className="flex items-center justify-between">
                   <p>Marketing</p>
-                  <div className="flex space-x-[7px] text-[hsla(230,10%,77%,1)] group-hover:text-white">
+                  <div className="flex space-x-[7px]">
                     <span className="text-xs">This Week</span>
                     <RxCaretDown />
                   </div>
                 </div>
-                <div className="flex justify-between">
+                <div className="flex justify-between mt-3">
                   {[
                     {
                       dotColor: "bg-primary",
@@ -137,11 +209,29 @@ export default function Home() {
                       text: "Retention",
                     },
                   ].map(({ dotColor, text }) => (
-                    <span key={text} className="flex space-x-1.5">
-                      <span className={cn("h-2 w-2", dotColor)}></span>
+                    <span key={text} className="flex space-x-1.5 items-center">
+                      <span
+                        className={cn("h-2 w-2 rounded-full", dotColor)}
+                      ></span>
                       <span>{text}</span>
                     </span>
                   ))}
+                </div>
+
+                <div className="mt-3 flex justify-center relative">
+                  <Doughnut
+                    data={doughnutData}
+                    width={205}
+                    height={205}
+                    options={{
+                      maintainAspectRatio: false,
+                      datasets: {
+                        doughnut: {
+                          borderWidth: 6,
+                        },
+                      },
+                    }}
+                  />
                 </div>
               </div>
               <div className="group transition rounded-lg bg-white hover:bg-primary px-[15px] py-[11px] flex flex-col justify-between">
@@ -198,7 +288,7 @@ export default function Home() {
               </div>
             </div>
 
-            <div className="w-full mt-[19px] px-5 py-4 rounded-lg bg-white min-h-[400px]">
+            <div className="w-full mt-[19px] px-5 py-4 rounded-lg bg-white">
               <div className="flex justify-between items-center">
                 <div className="flex space-x-5 items-center">
                   <p className="font-medium">Summary</p>
@@ -212,10 +302,13 @@ export default function Home() {
                   <RxCaretDown />
                 </div>
               </div>
+              <div className="mt-4">
+                <Bar options={barOptions} data={barData} />
+              </div>
             </div>
           </div>
 
-          <div className="h-full max-h-full overflow-auto rounded-lg bg-white p-5">
+          <div className="md:col-span-2 lg:col-span-1 h-full max-h-full overflow-auto rounded-lg bg-white p-5">
             <p className="font-medium">Recent Orders</p>
             <div className="flex flex-col mt-3">
               {Array.from({ length: 8 }).map((item, index) => {
@@ -227,15 +320,15 @@ export default function Home() {
                         height={49}
                         width={49}
                         alt="iphone1"
-                        className="mr-[14px]"
+                        className="mr-[14px] object-cover"
                       />
-                      <div className="flex grow justify-between">
+                      <div className="flex flex-col md:flex-row grow justify-between">
                         <div className="flex flex-col">
                           <p className="text-sm mb-[10px]">iPhone 13</p>
                           <p className="text-sm font-medium">₦730,000.00 x 1</p>
                         </div>
-                        <div className="flex flex-col items-end">
-                          <p className="text-sm mb-[10px] text-gray">
+                        <div className="flex justify-between md:flex-col items-center md:items-end mt-2">
+                          <p className="text-sm md:mb-[10px] text-gray">
                             12 Sept 2022
                           </p>
                           <p className="text-xs rounded-full text-[hsla(137,32%,47%,1)] bg-[hsla(158,49%,39%,0.12)] py-0.5 px-4">
@@ -250,14 +343,14 @@ export default function Home() {
                         height={49}
                         width={49}
                         alt="iphone2"
-                        className="mr-3.5"
+                        className="mr-3.5 object-cover"
                       />
-                      <div className="flex grow justify-between">
+                      <div className="flex flex-col md:flex-row grow justify-between">
                         <div className="flex flex-col">
                           <p className="text-sm mb-2.5">iPhone 13</p>
                           <p className="text-sm font-medium">₦730,000.00 x 1</p>
                         </div>
-                        <div className="flex flex-col items-end">
+                        <div className="flex justify-between md:flex-col items-center md:items-end mt-2">
                           <p className="text-sm mb-3 text-gray">12 Sept 2022</p>
                           <p className="text-xs rounded-full text-[hsla(0,52%,59%,1)] bg-[hsla(4,87%,71%,0.12)] py-0.5 px-4">
                             Pending
--- src/app/template.tsx
+++ src/app/template.tsx
@@ -1,16 +1,20 @@
 "use client";
 
+import { ContactIcon } from "@/components/icons/contact-icon";
 import { ConversationsIcon } from "@/components/icons/conversations-icon";
 import { CustomersIcon } from "@/components/icons/customers-icon";
 import { DashboardIcon } from "@/components/icons/dashboard-icon";
+import { GiftIcon } from "@/components/icons/gift-icon";
 import { InventoryIcon } from "@/components/icons/inventory-icon";
 import { Logo } from "@/components/icons/logo";
+import { LogoutIcon } from "@/components/icons/logout-icon";
 import { NotificationsIcon } from "@/components/icons/notifications-icon";
 import { OrdersIcon } from "@/components/icons/orders-icon";
 import { SettingsIcon } from "@/components/icons/settings-icon";
 import { Button } from "@/components/ui/button";
 import { ScrollArea } from "@/components/ui/scroll-area";
 import { useResponsive } from "@/lib/hooks/use-responsive";
+import useWindowSize from "@/lib/hooks/use-window-size";
 import { ROUTES } from "@/lib/routes";
 import { cn } from "@/lib/utils";
 import Image from "next/image";
@@ -18,25 +22,27 @@ import Link from "next/link";
 import { usePathname } from "next/navigation";
 import React, { ReactNode, useEffect, useState } from "react";
 import { BsArrowBarLeft, BsArrowBarRight } from "react-icons/bs";
-import { RxCaretDown } from "react-icons/rx";
+import { RxCaretDown, RxCaretRight } from "react-icons/rx";
 
 const Template = ({ children }: { children: ReactNode }) => {
   const currentPath = usePathname();
 
   const [isSidebarOpen, setIsSidebarOpen] = useState(true);
-  const { isBigScreen } = useResponsive() || {};
+  const { width } = useWindowSize();
+
+  const allowFullNav = width ? width >= 1280 : undefined;
 
   useEffect(() => {
-    if (typeof isBigScreen !== "undefined" && !isBigScreen) {
-      setIsSidebarOpen(isBigScreen);
+    if (typeof allowFullNav !== "undefined" && !allowFullNav) {
+      setIsSidebarOpen(allowFullNav);
     }
-  }, [isBigScreen]);
+  }, [allowFullNav]);
 
   const closeSidebar = () => {
     setIsSidebarOpen(false);
   };
 
-  const showFullItem = isSidebarOpen || !isBigScreen;
+  const showFullItem = isSidebarOpen || !allowFullNav;
 
   const currentRoute = Object.values(ROUTES).find(
     (route) => route.path === currentPath
@@ -46,17 +52,17 @@ const Template = ({ children }: { children: ReactNode }) => {
     <main className="flex h-screen max-h-screen min-h-screen min-w-screen relative overflow-hidden">
       <div
         className={cn(
-          "fixed lg:relative h-full min-h-full max-h-full bg-white shrink-0 z-50",
+          "fixed xl:relative h-full min-h-full max-h-full bg-white shrink-0 z-50",
           showFullItem ? "w-[240px]" : "w-auto",
           {
-            "-translate-x-full": !isSidebarOpen && !isBigScreen,
+            "-translate-x-full": !isSidebarOpen && !allowFullNav,
           }
         )}
       >
         <div
           className={cn(
             "flex flex-col h-full min-h-full max-h-full overflow-hidden",
-            showFullItem ? "px-8" : "px-4"
+            showFullItem ? "px-3" : "px-2"
           )}
         >
           <Link href={"#"} className="mt-6 flex items-center">
@@ -68,76 +74,112 @@ const Template = ({ children }: { children: ReactNode }) => {
             ) : null}
           </Link>
 
-          <nav className={cn("flex flex-col space-y-3 mt-[62px]")}>
-            {[
-              {
-                Icon: DashboardIcon,
-                text: "Dashboard",
-                href: ROUTES.DASHBOARD.path,
-              },
-              {
-                Icon: OrdersIcon,
-                text: "Orders",
-                href: ROUTES.ORDERS.path,
-                count: 3,
-              },
-              {
-                Icon: CustomersIcon,
-                text: "Customers",
-                href: ROUTES.CUSTOMERS.path,
-              },
-              {
-                Icon: InventoryIcon,
-                text: "Inventory",
-                href: ROUTES.INVENTORY.path,
-              },
-              {
-                Icon: ConversationsIcon,
-                text: "Conversations",
-                href: ROUTES.CONVERSATIONS.path,
-                count: 16,
-              },
-              {
-                Icon: SettingsIcon,
-                text: "Settings",
-                href: ROUTES.SETTINGS.path,
-              },
-            ].map(({ Icon, text, href, count }) => {
-              const isActive = currentPath === href;
-              return (
-                <Link
-                  href={href}
-                  key={text}
-                  className={cn(
-                    "flex group items-center rounded-xl text-[hsla(230,5%,34%,1)] hover:bg-primary hover:text-white cursor-pointers px-3.5 py-3",
-                    isActive ? "text-white bg-primary" : ""
-                  )}
-                  onClick={() => {
-                    if (!isBigScreen) {
-                      closeSidebar();
-                    }
-                  }}
-                >
-                  <Icon className="shrink-0" />
-                  {showFullItem ? (
-                    <div className="flex items-center ml-4 space-x-3">
-                      <span
-                        className={cn("text-sm", {
-                          "text-white": isActive,
-                        })}
-                      >
-                        {text}
-                      </span>
-                      {count ? (
-                        <span className="w-6 h-6 grid place-items-center text-xs rounded-full text-[hsla(230,9%,12%,1)] bg-yellow">
-                          {count}
+          <nav
+            className={cn(
+              "flex flex-col justify-between h-full space-y-3 mt-[40px] pb-4"
+            )}
+          >
+            <div className="flex flex-col">
+              {[
+                {
+                  Icon: DashboardIcon,
+                  text: "Dashboard",
+                  href: ROUTES.DASHBOARD.path,
+                },
+                {
+                  Icon: OrdersIcon,
+                  text: "Orders",
+                  href: ROUTES.ORDERS.path,
+                  count: 3,
+                },
+                {
+                  Icon: CustomersIcon,
+                  text: "Customers",
+                  href: ROUTES.CUSTOMERS.path,
+                },
+                {
+                  Icon: InventoryIcon,
+                  text: "Inventory",
+                  href: ROUTES.INVENTORY.path,
+                },
+                {
+                  Icon: ConversationsIcon,
+                  text: "Conversations",
+                  href: ROUTES.CONVERSATIONS.path,
+                  count: 16,
+                },
+                {
+                  Icon: SettingsIcon,
+                  text: "Settings",
+                  href: ROUTES.SETTINGS.path,
+                },
+              ].map(({ Icon, text, href, count }) => {
+                const isActive = currentPath === href;
+                return (
+                  <Link
+                    href={href}
+                    key={text}
+                    className={cn(
+                      "flex group items-center rounded-xl text-[hsla(230,5%,34%,1)] hover:bg-primary hover:text-white cursor-pointers px-3.5 py-3",
+                      isActive ? "text-white bg-primary" : ""
+                    )}
+                    onClick={() => {
+                      if (!allowFullNav) {
+                        closeSidebar();
+                      }
+                    }}
+                  >
+                    <Icon className="shrink-0" />
+                    {showFullItem ? (
+                      <div className="flex items-center ml-4 space-x-3">
+                        <span
+                          className={cn("text-sm", {
+                            "text-white": isActive,
+                          })}
+                        >
+                          {text}
                         </span>
-                      ) : null}
-                    </div>
+                        {count ? (
+                          <span className="w-6 h-6 grid place-items-center text-xs rounded-full text-[hsla(230,9%,12%,1)] bg-yellow">
+                            {count}
+                          </span>
+                        ) : null}
+                      </div>
+                    ) : null}
+                  </Link>
+                );
+              })}
+            </div>
+            <div className="flex flex-col space-y-3">
+              <div className="flex rounded-lg items-center space-x-4 px-3.5 py-3 bg-[hsla(202,4%,39%,0.1)]">
+                <ContactIcon />
+                {showFullItem ? (
+                  <span className="text-sm">Contact Support</span>
+                ) : null}
+              </div>
+              <div className="px-3.5 rounded-lg py-3 bg-[hsla(32,100%,78%,0.2)]">
+                <div className="flex space-x-4 items-center ">
+                  <GiftIcon className="shrink-0" />
+                  {showFullItem ? (
+                    <span className="text-sm">Free Gift Awaits You!</span>
                   ) : null}
-                </Link>
-              );
-            })}
+                </div>
+                {showFullItem ? (
+                  <div className="flex items-center text-[hsla(230,5%,45%,1)] mt-2.5">
+                    <p className="text-xs">Upgrade your account</p>
+                    <RxCaretRight className="ml-4 shrink-0" size={16} />
+                  </div>
+                ) : null}
+              </div>
+              <div
+                className={cn("space-x-4 flex text-[hsla(0,52%,59%,1)]", {
+                  "justify-center": !showFullItem,
+                })}
+              >
+                <LogoutIcon className="" />
+                {showFullItem ? <span>Logout</span> : null}
+              </div>
+            </div>
           </nav>
         </div>
         <Button
@@ -145,7 +187,7 @@ const Template = ({ children }: { children: ReactNode }) => {
           className={cn(
             "absolute rounded-full p-0 h-[40px] w-[40px] right-0 translate-x-2/4 top-[10px] z-50",
             {
-              hidden: !isSidebarOpen && !isBigScreen,
+              hidden: !isSidebarOpen && !allowFullNav,
             }
           )}
         >
@@ -163,18 +205,18 @@ const Template = ({ children }: { children: ReactNode }) => {
           )}
         >
           {!isSidebarOpen ? (
-            <div className="inline-flex">
-              <Link href={ROUTES.DASHBOARD.path} className="lg:hidden">
+            <div className="inline-flex mr-3">
+              <Link href={ROUTES.DASHBOARD.path} className="xl:hidden">
                 <Logo width={36} height={36} />
               </Link>
               <Button
                 onClick={() => setIsSidebarOpen((prev) => !prev)}
-                className="rounded-full bg-transparent inline-flex p-0 border border-placeholder lg:hidden h-[40px] w-[40px] ml-2.5"
+                className="rounded-full bg-transparent inline-flex p-0 border border-placeholder xl:hidden h-[40px] w-[40px] ml-2.5"
               >
                 {isSidebarOpen ? (
-                  <BsArrowBarLeft className="text-placeholder hover:text-white w-[18px] h-[18px]" />
+                  <BsArrowBarLeft className="text-gray hover:text-white w-[18px] h-[18px]" />
                 ) : (
-                  <BsArrowBarRight className="text-placeholder hover:text-white w-[18px] h-[18px]" />
+                  <BsArrowBarRight className="text-gray hover:text-white w-[18px] h-[18px]" />
                 )}
               </Button>
             </div>
@@ -187,11 +229,11 @@ const Template = ({ children }: { children: ReactNode }) => {
             ) : null}
 
             <div className="space-x-5 flex items-center ml-auto">
-              <div className="flex rounded-lg px-3 py-1.5 space-x-5 text-[hsla(230,9%,12%,1)] bg-yellow-accent text-sm">
+              <div className="flex rounded-lg items-center px-3 py-1.5 space-x-5 text-[hsla(230,9%,12%,1)] bg-yellow-accent text-sm">
                 <span>Nanny’s Shop</span>
-                <RxCaretDown />
+                <RxCaretDown className="shrink-0" />
               </div>
-              <NotificationsIcon />
+              <NotificationsIcon className="shrink-0" />
               <Image
                 src="/assets/images/profile.png"
                 height={32}
@@ -201,7 +243,6 @@ const Template = ({ children }: { children: ReactNode }) => {
               />
             </div>
           </div>
-
         </div>
         <div className="pl-[21px]">stuff</div>
         <ScrollArea className="bg-[hsla(230,33%,97%,1)] grow px-[19px] pt-[28px] pb-5 flex flex-col overflow-y-auto">
